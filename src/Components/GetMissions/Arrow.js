a05db845abbe7b00ad868a41b7a1d617134a6ed9
Robson Melo de Souza <sys.robson@gmail.com>
2022-07-26 13:29:54 -0300

    added lazy loading and sorting, still to improve those features

---
+++ src/Components/GetMissions/Arrow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Arrow.css'
+
+function Arrow() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="arrow" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+  <path strokeLinecap="round" strokeLinejoin="round" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
+</svg>
+  )
+}
+
+export default Arrow
--- src/Components/GetMissions/GetMissions.js
+++ src/Components/GetMissions/GetMissions.js
@@ -1,39 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery, gql } from "@apollo/client";
 import Error from "../Error/Error";
 import Loader from "../Loader/Loader";
 import "./GetMissions.css";
+import Arrow from './Arrow.js'
 
 function GetMissions() {
-  //query to get all the mission information for the missions list display
-  const GET_MISSIONS = gql`
-    {
-      launchesPast(limit: 10) {
-        mission_name
-        rocket {
-          rocket_name
-          rocket_type
-        }
-        launch_year
-      }
-    }
-  `;
+
+  const [limit, setLimit] = useState(6)
+  const [sort, setSort] = useState(' ')
+   //query to get all the mission information for the missions list display
+   const GET_MISSIONS = gql`
+   {
+     launchesPast(limit: ${limit} ${sort}) {
+       mission_name
+       rocket {
+         rocket_name
+         rocket_type
+       }
+       launch_year
+     }
+   }
+   `;
   const { data, loading, error } = useQuery(GET_MISSIONS);
   if (loading) return <Loader />;
   if (error) return <Error error={error} />;
+
+  function loadMore(e){
+    e.preventDefault();
+    setLimit(limit+6)
+  }
+
+  function sortResult(e){
+    e.preventDefault();
+    setSort(', sort: "mission_name"')
+
+  }
+  console.log(data.launchesPast)
 
   return (
     <div className="missions-table">
       <div className="Missions">
         <div className="mission-header">
-          <h4 className="mission-header-cell">MISSION NAME</h4>
+          <h4 className="mission-header-cell">MISSION NAME <span onClick={sortResult}><Arrow/></span></h4>
           <h4 className="mission-header-cell">ROCKET NAME</h4>
           <h4 className="mission-header-cell center">ROCKET TYPE</h4>
           <h4 className="mission-header-cell center">LAUNCH YEAR</h4>
         </div>
 
         {data.launchesPast.map((mission) => {
-          console.log(mission);
           return (
             <div className="mission">
               <div className="cell">{mission.mission_name}</div>
@@ -43,6 +58,10 @@ function GetMissions() {
             </div>
           );
         })}
+        <div className="load-more">
+          <p>{limit} of {data.launchesPast.length}</p>
+        <button className="btn-primary" onClick={loadMore}>Load More</button>
+        </div>
       </div>
     </div>
   );
